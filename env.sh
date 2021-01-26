a71339bc9ca74155f229dcaa8241657c7ea367e4
Jason P. Leasure <jpleasu@gmail.com>
2021-01-26 16:49:23 -0500

    env.sh now deletes graalvm symlink before (re)install and cleans up tarball

--- env.sh
+++ env.sh
@@ -1,12 +1,15 @@
 D="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd -P "$( dirname "$SOURCE" )" && pwd )"
  
 ver=21.0.0
-if [ ! -d $D/graalvm-ce-java11-${ver} ]; then
-  pushd $D
-  wget https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${ver}/graalvm-ce-java11-linux-amd64-${ver}.tar.gz
-  tar -zxvf graalvm-ce-java11-linux-amd64-${ver}.tar.gz
-  ln -s graalvm-ce-java11-${ver} graalvm
+tarball="graalvm-ce-java11-linux-amd64-${ver}.tar.gz"
+if [ ! -d "$D/graalvm-ce-java11-${ver}" ]; then
+  pushd "$D"
+  rm -f graalvm
+  wget "https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-${ver}/${tarball}"
+  tar -zxvf "${tarball}"
+  ln -s "graalvm-ce-java11-${ver}" graalvm
   ./graalvm/bin/gu install llvm-toolchain native-image python ruby R
+  rm -f "${tarball}"
   popd
 fi
  
